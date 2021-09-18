9cc2c26ed58fe5c0177f14bda4ddcefcf6d49dca
Javinator11 <sosa.puga.javier@gmail.com>
2021-09-18 11:55:38 -0600

    Retos

--- product-app1/src/components/ProductInfo/index.tsx
+++ product-app1/src/components/ProductInfo/index.tsx
@@ -1,13 +1,17 @@
 import "./index.css";
-import { Grid, Paper, Typography } from "@material-ui/core";
+import { Grid, Paper, Typography, Select, MenuItem, InputLabel, Button } from "@material-ui/core";
 
+/**
+ * Product info elements
+ * @returns ProductInfo UI elements
+ */
 const ProductInfo=()=>{
     return (
         <div className="productInfo">
             <Grid container className="productGrid" spacing={2}>
               <Grid item lg={4}>
                 <Paper className="largeImage" >
-                  <img src="https://dummyimage.com/500x500/000/0011ff" />
+                  <img src="https://dummyimage.com/400x400/000/0011ff" alt="Loli's 501 Original Fit Jeans Jeans para Hombre" />
                 </Paper>
               </Grid>
 
@@ -31,7 +35,56 @@ const ProductInfo=()=>{
                   <Typography className="dollars">
                     706.93
                   </Typography>                
+                </Grid>
+                <Grid item lg={8}/>     
+                <Grid item lg={2}>
+                  <InputLabel id="color-label" className="productLabel">Color</InputLabel>
+                  <Select
+                    labelId="color-label"
+                    id="color-select"
+                    label="Color"
+                  >
+                  <MenuItem value="BLK">Black</MenuItem>
+                  <MenuItem value="BLU">Blue</MenuItem>
+                  <MenuItem value="BWN">Brown</MenuItem>
+                  </Select>
+                </Grid>
+
+                <Grid item lg={2}>
+                  <InputLabel id="size-label" className="productLabel">Size</InputLabel>
+                  <Select
+                    labelId="size-label"
+                    id="size-select"
+                    label="Size"
+                  >
+                  <MenuItem value="30x32">30x32</MenuItem>
+                  <MenuItem value="33x30">33x30</MenuItem>
+                  <MenuItem value="32x32">32x32</MenuItem>
+                  <MenuItem value="34x32">34x32</MenuItem>
+                  <MenuItem value="36x32">36x32</MenuItem>
+                  </Select>
                 </Grid> 
+                
+                <Grid item lg={8}/>
+                <Grid item lg={2}>
+                  <InputLabel id="quantity-label" className="productLabel">Quantity</InputLabel>
+                  <Select
+                    labelId="quantity-label"
+                    id="quantity-select"
+                    label="quantity"
+                  >
+                  <MenuItem value="1">1</MenuItem>
+                  <MenuItem value="2">2</MenuItem>
+                  <MenuItem value="3">3</MenuItem>
+                  <MenuItem value="4">4</MenuItem>
+                  <MenuItem value="5">5</MenuItem>
+                  </Select>
+                </Grid>
+                <Grid item lg={10}/>
+
+                <Grid item lg={4}>
+                  <Button variant="contained" className="addCartButton">Add to cart</Button>
+                </Grid>
                 <Grid item lg={12}/>                               
               </Grid>
             </Grid>
